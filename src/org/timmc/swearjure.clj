ad7ca079334ca4bcd27923f0bfdce906991d6de8
Tim McCormack <cortex@brainonfire.net>
2013-01-19 01:00:01 -0500

    Rewrite for multimethod dispatch.

--- src/org/timmc/swearjure.clj
+++ src/org/timmc/swearjure.clj
@@ -17,32 +17,50 @@
         (pos? n) (cons '+ (repeat n '(*)))
         :else (list '- (compile-number (- n)))))
 
+(defn form-dispatch
+  [form]
+  (cond (integer? form) :integer
+        (keyword? form) :lit
+        (symbol? form) :lit
+        (vector? form) :vector
+        (map? form) :map
+
+        (sequential? form)
+        (cond (empty? form) :lit
+              (symbol? (first form)) (keyword (first form)))))
+
+(defmulti ^:private c-form "Slightly more raw form of compile-form."
+  (fn [_ form] (form-dispatch form)))
+
 (defn compile-form
+  "Compile a form in a descent-scope into something suitable for a function
+body."
   [scope form]
   (->
-   (cond (integer? form)
-         {:body (compile-number form)}
-
-         (keyword? form)
-         {:body form}
-
-         (symbol? form)
-         {:body form}
-
-         (vector? form)
-         (let [raw (map #(compile-form (conj scope 'v) %) form)
-               [bodies helpers] (transpose (map (juxt :body :helpers) raw))]
-           {:body (vec bodies)
-            :helpers (apply merge helpers)})
-
-         (map? form)
-         (let [[ks vs] (transpose form)
-               cks (compile-form (conj scope 'mk) ks)
-               cvs (compile-form (conj scope 'mv) vs)]
-           {:body (into {} (map vector (:body cks) (:body cvs)))
-            :helpers (merge (:helpers cks) (:helpers cvs))}))
+   (c-form scope form)
    (assoc :scope scope)
    (update-in [:helpers] (fnil identity {}))))
+
+(defmethod c-form :lit [scope form]
+  {:scope scope, :body form})
+
+(defmethod c-form :integer [scope form]
+  {:scope scope, :body (compile-number form)})
+
+(defmethod c-form :vector [scope form]
+  (let [raw (map #(c-form (conj scope 'v) %) form)
+        [bodies helpers] (transpose (map (juxt :body :helpers) raw))]
+    {:scope scope
+     :body (vec bodies)
+     :helpers (apply merge helpers)}))
+
+(defmethod c-form :map [scope form]
+  (let [[ks vs] (transpose form)
+        cks (c-form (conj scope 'mk) ks)
+        cvs (c-form (conj scope 'mv) vs)]
+    {:scope scope
+     :body (into {} (map vector (:body cks) (:body cvs)))
+     :helpers (merge (:helpers cks) (:helpers cvs))}))
 
 ;; This is what an input should look like.
 #_
