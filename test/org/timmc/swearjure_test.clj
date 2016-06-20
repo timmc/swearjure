b7a41fe3c89059925ba9739749c7d30c208d9bf8
Tim McCormack <cortex@brainonfire.net>
2016-06-19 21:13:13 -0400

    Don't pollute test ns with src ns

--- test/org/timmc/swearjure_test.clj
+++ test/org/timmc/swearjure_test.clj
@@ -1,53 +1,52 @@
 (ns org.timmc.swearjure-test
-  (:refer-clojure :exclude (compile))
-  (:use clojure.test
-        org.timmc.swearjure))
+  (:use clojure.test)
+  (:require [org.timmc.swearjure :as s]))
 
 (deftest form-builders
-  (is (= (eval ev-true) true))
-  (is (= (eval ev-false) false))
-  (is (= (eval ev-nil) nil))
-  (is (= (map (comp eval wrap-boolean)
+  (is (= (eval s/ev-true) true))
+  (is (= (eval s/ev-false) false))
+  (is (= (eval s/ev-nil) nil))
+  (is (= (map (comp eval s/wrap-boolean)
               [nil false true 1 [1 2]])
          [false false true true true])))
 
 (deftest naming
-  (is (= (scope-name '[foo bar baz]) "foo-bar-baz")))
+  (is (= (s/scope-name '[foo bar baz]) "foo-bar-baz")))
 
 (deftest literals
-  (are [i o] (= (compile-number i) o)
+  (are [i o] (= (s/compile-number i) o)
        0 '(+)
        3 '(+ (*) (*) (*))
        -2 '(- (+ (*) (*)))))
 
 (deftest combining-forms
   (testing "vecs"
-    (is (= (compile-form '[top] [])
+    (is (= (s/compile-form '[top] [])
            {:body []
             :helpers {}
             :scope '[top]}))
-    (is (= (compile-form '[top] '[:_ [1]])
+    (is (= (s/compile-form '[top] '[:_ [1]])
            {:body '[:_ [(+ (*))]]
             :helpers {}
             :scope '[top]})))
   (testing "maps"
-    (is (= (compile-form '[top] '{:_ [1]})
+    (is (= (s/compile-form '[top] '{:_ [1]})
            {:body '{:_ [(+ (*))]}
             :helpers {}
             :scope '[top]}))))
 
 (deftest convenience ;; or sanity
   (testing "inc and dec"
-    (is (= (compile-form [] '[(inc (dec foo))])
+    (is (= (s/compile-form [] '[(inc (dec foo))])
            {:body '[(+ (- foo (*)) (*))]
             :helpers {}
             :scope []})))
   (testing "if"
-    (is (= (with-redefs [unique #(symbol (str % "_17"))]
-             (compile-form '[top] '(if [:test 0] [:then 1] [:else 2])))
+    (is (= (with-redefs [s/unique #(symbol (str % "_17"))]
+             (s/compile-form '[top] '(if [:test 0] [:then 1] [:else 2])))
            {:body (list :org.timmc.swearjure/call-helper
-                        (list {ev-true 'top-ift_17}
-                              (wrap-boolean '[:test (+)])
+                        (list {s/ev-true 'top-ift_17}
+                              (s/wrap-boolean '[:test (+)])
                               'top-iff_17))
             :helpers {'top-ift_17 '[:then (+ (*))]
                       'top-iff_17 '[:else (+ (*) (*))]}
